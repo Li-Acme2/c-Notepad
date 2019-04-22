3cde20716cb90b85ce32a51753b5d26b17f61370
Li-Acme2 <ly1379ly@outlook.com>
2019-04-22 10:54:49 +0800

    添加项目文件。

---
+++ Notepad1/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Notepad1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.buttonEdit1 = new DevExpress.XtraEditors.CheckEdit();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.comboBox2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox2.Font = new System.Drawing.Font("宋体", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "9",
+            "11",
+            "13",
+            "15",
+            "17",
+            "19",
+            "21",
+            "23",
+            "25"});
+            this.comboBox2.Location = new System.Drawing.Point(446, 15);
+            this.comboBox2.MaxLength = 2;
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(80, 23);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.Text = "字号";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            this.comboBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBox2_KeyDown);
+            this.comboBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.comboBox2_KeyPress);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.comboBox3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox3.Font = new System.Drawing.Font("宋体", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(303, 15);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(137, 23);
+            this.comboBox3.TabIndex = 1;
+            this.comboBox3.Text = "字体";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            this.comboBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.comboBox3_KeyPress);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 10;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.button2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox3, 5, 0);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox2, 6, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 8, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 54);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = global::Notepad1.Properties.Resources.保存;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.Location = new System.Drawing.Point(123, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(54, 48);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "保存";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Image = global::Notepad1.Properties.Resources.加粗;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button3.Location = new System.Drawing.Point(183, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(54, 48);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "加粗";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Image = global::Notepad1.Properties.Resources.倾斜;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button4.Location = new System.Drawing.Point(243, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(54, 48);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "倾斜";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::Notepad1.Properties.Resources.打开;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(63, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(54, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 54);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 396);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // buttonEdit1
+            // 
+            this.buttonEdit1.EditValue = null;
+            this.buttonEdit1.Location = new System.Drawing.Point(406, 208);
+            this.buttonEdit1.Name = "buttonEdit1";
+            this.buttonEdit1.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.Style3D;
+            this.buttonEdit1.Properties.Caption = "checkEdit1";
+            this.buttonEdit1.Size = new System.Drawing.Size(100, 23);
+            this.buttonEdit1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(532, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 54);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button5.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
+            this.button5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.Location = new System.Drawing.Point(3, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(54, 48);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "新建";
+            this.button5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.buttonEdit1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "文本编辑器";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private DevExpress.XtraEditors.CheckEdit buttonEdit1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Notepad1/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Drawing.Text;
+using System.Collections;
+using System.IO;
+
+namespace Notepad1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "文本编辑器";
+            this.label1.Visible = false;
+        }
+
+        private void comboBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //取消编辑模式
+            //e.Handled = true;//与DropDownStyle的DropDown连用产生显示文本，但只能下拉无法编辑的状态
+        }
+        //加粗
+        private void button3_Click(object sender, EventArgs e)
+        {//普通-》加粗；加粗-》普通
+            if (richTextBox1.Font.Bold == false)
+            {
+                richTextBox1.Font = new Font(richTextBox1.Font, richTextBox1.Font.Style | FontStyle.Bold);
+            }
+            else
+            {
+                richTextBox1.Font = new Font(richTextBox1.Font, richTextBox1.Font.Style & FontStyle.Regular);
+            }
+        }
+
+        private void comboBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //键入不是数字，也不是enter，也不是删除键
+            if (!(Char.IsNumber(e.KeyChar)) && e.KeyChar != (char)13 && e.KeyChar != (char)8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (button1.Image != null)
+            {//将图片按要求缩小或者放大
+                //1.获取要改变的图片；2.改变图片的宽度；3.改变图片的高度
+                button1.Image = new Bitmap(button1.Image, button1.Image.Width - 100, button1.Image.Height - 70);
+            }
+            if (button2.Image != null)
+            {//将图片按要求缩小或者放大
+                //1.获取要改变的图片；2.改变图片的宽度；3.改变图片的高度
+                button2.Image = new Bitmap(button2.Image, button2.Image.Width - 100, button2.Image.Height - 101);
+            }
+            if (button3.Image != null)
+            {//将图片按要求缩小或者放大
+                //1.获取要改变的图片；2.改变图片的宽度；3.改变图片的高度
+                button3.Image = new Bitmap(button3.Image, button3.Image.Width - 100, button3.Image.Height - 101);
+            }
+            if (button4.Image != null)
+            {//将图片按要求缩小或者放大
+                //1.获取要改变的图片；2.改变图片的宽度；3.改变图片的高度
+                button4.Image = new Bitmap(button4.Image, button4.Image.Width - 50, button4.Image.Height - 75);
+            }
+            if (button5.Image != null)
+            {
+                button5.Image = new Bitmap(button5.Image, button5.Image.Width - 73, button5.Image.Height - 100);
+            }
+            //窗体加载时，加载系统字体
+            InstalledFontCollection myfont = new InstalledFontCollection();
+            FontFamily[] ff = myfont.Families;
+            ArrayList list = new ArrayList();
+            int count = ff.Length;
+            for (int i = 0; i < count; i++)
+            {
+                string fontname = ff[i].Name;
+                comboBox3.Items.Add(fontname);
+            }
+        }
+        //倾斜
+        private void button4_Click(object sender, EventArgs e)
+        {//判断是否倾斜
+            if (richTextBox1.Font.Italic == false)
+            {//未倾斜就加倾斜
+                richTextBox1.Font = new Font(richTextBox1.Font, richTextBox1.Font.Style | FontStyle.Italic);
+            }
+            else
+            {//倾斜就取消
+                richTextBox1.Font = new Font(richTextBox1.Font, richTextBox1.Font.Style & FontStyle.Regular);
+            }
+        }
+        //字号
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int fs = Convert.ToInt32(comboBox2.Text);
+            richTextBox1.Font = new Font(richTextBox1.Font.FontFamily, fs, richTextBox1.Font.Style);
+        }
+
+        private void comboBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                this.comboBox2_SelectedIndexChanged(sender, e);
+            }
+        }
+        //修改字体
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string fontname = comboBox3.Text;
+            richTextBox1.Font = new Font(fontname, richTextBox1.Font.Size, richTextBox1.Font.Style);
+        }
+        //保存
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (richTextBox1.Text.Trim() != "")
+            {
+                if (this.Text == "文本编辑器")
+                {
+                    //创建一个筛选器/过滤器
+                    saveFileDialog1.Filter = ("文本文档（*.txt）|*.txt");//只能存.txt格式的
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        string sfilepath = saveFileDialog1.FileName;
+                        //保存文件到指定路径
+                        StreamWriter sw = new StreamWriter(sfilepath, false);
+                        sw.WriteLine(richTextBox1.Text.Trim());
+                        //将保存的路径设置为当前窗体名称
+                        this.Text = sfilepath;
+                        //释放资源
+                        sw.Flush();
+                        sw.Close();
+                    }
+                }
+                else
+                {
+
+                    string path = this.Text;
+                    StreamWriter sw = new StreamWriter(path, false);
+                    sw.WriteLine(richTextBox1.Text.Trim());
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("空文档无法保存！", "信息提示", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+            }
+        }
+        //打开
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = ("文本文档（*.txt）|*.txt");
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog1.FileName;
+                StreamReader sr = new StreamReader(path, Encoding.UTF8);
+                string text = sr.ReadToEnd();
+                richTextBox1.Text = text;
+                //将打开文本的路径显示在窗体名称，方便之后保存
+                this.Text = path;
+                sr.Close();
+            }
+        }
+        //窗体关闭时提示
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //判断记号状态
+            if (this.label1.Text == "*")
+            {
+                DialogResult dr = MessageBox.Show("文档已被编辑但未保存，是否继续退出？", "信息询问", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dr == System.Windows.Forms.DialogResult.Yes)
+                {
+                    this.Dispose();
+                    button2_Click(sender, e);
+                }
+                else
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+        //文本被编辑
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.label1.Text = "*";
+        }
+        //新建
+        private void button5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            this.label1.Text = "";
+            this.Text = "";
+        }
+    }
+}
